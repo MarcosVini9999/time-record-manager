197a837d4aa629f2bb820d8eb8e928b1df6408ac
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-08-31 20:08:09 -0300

    feat: dialog styles

--- src/pages/TimeRecordPage/TimeRecordPage.tsx
+++ src/pages/TimeRecordPage/TimeRecordPage.tsx
@@ -14,6 +14,7 @@ import { Fragment, useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import clock from "@/assets/icons/clock.svg";
 import { gql, useMutation } from "@apollo/client";
+import moment from "moment";
 
 interface OutletContextProps {
   userTimes: IUserTimes;
@@ -115,6 +116,8 @@ export const TimeRecordPage = () => {
         sx={{
           "& .MuiDialog-paper": {
             backgroundColor: Colors.white,
+            maxWidth: "400px",
+            margin: "0px",
             "& .MuiDialogTitle-root": {
               color: "#20292E",
             },
@@ -124,19 +127,79 @@ export const TimeRecordPage = () => {
           },
         }}
       >
-        <DialogTitle id="alert-dialog-title">Registrar novo ponto</DialogTitle>
-        <DialogContent>
-          <img src={clock} alt="Imagem de relógio" />
-          <DialogContentText id="alert-dialog-description">{Date()}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <PontoGoButton onClick={clockIN} buttonStyle="primary">
+        <Box
+          sx={{
+            paddingBlock: "60px 25px",
+            paddingInline: {
+              sm: "45px",
+              md: "92px",
+            },
+          }}
+        >
+          <Typography fontWeight="700" fontSize="20px" lineHeight="30px" textAlign="center">
+            Registrar novo ponto
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <img src={clock} alt="Imagem de relógio" style={{ marginBottom: "10px" }} />
+          <DialogContentText id="alert-dialog-description">
+            <Typography
+              fontWeight="700"
+              fontSize="30px"
+              lineHeight="45px"
+              color={Colors.primary}
+              textAlign="center"
+            >
+              {moment(Date.now()).locale("pt-br").format("HH:mm")}
+            </Typography>
+            <Typography
+              fontWeight="400"
+              fontSize="16px"
+              lineHeight="24px"
+              color={Colors.primary}
+              sx={{ opacity: "50%" }}
+              textAlign="center"
+            >
+              {moment(Date.now()).locale("pt-br").format("DD/MM/YYYY")}
+            </Typography>
+          </DialogContentText>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingBottom: "60px",
+          }}
+        >
+          <PontoGoButton
+            onClick={clockIN}
+            buttonStyle="primary"
+            sx={{
+              backgroundColor: `${Colors.primary} !important`,
+              marginBottom: "10px",
+            }}
+          >
             Bater ponto
           </PontoGoButton>
-          <PontoGoButton onClick={handleClose} buttonStyle="secondary">
+          <PontoGoButton
+            onClick={handleClose}
+            buttonStyle="borders"
+            sx={{
+              border: `1px solid ${Colors.primary} !important`,
+              color: `${Colors.primary} !important`,
+            }}
+          >
             Cancelar
           </PontoGoButton>
-        </DialogActions>
+        </Box>
       </Dialog>
     </Fragment>
   );
